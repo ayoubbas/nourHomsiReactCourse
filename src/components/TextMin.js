1dc666b428513b6becd1c3730ecc26b86936eb52
ayoub <ayoub-ok147@hotmail.fr>
2023-12-11 22:59:48 +0100

    some

--- src/components/TextMin.js
+++ src/components/TextMin.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function TextMin (){
-    return (
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus facere aperiam iste mollitia doloribus ad, quod odit natus amet sapiente, laudantium libero molestiae doloremque cumque neque nihil sed eveniet perferendis.</p>
-
-    )
-
+function TextMin() {
+  const client = "programmer";
+  return (
+    <p>
+      <span className={`${(client === "designer" ? "colory" : "")}`}>Graphic</span>{" "}
+      ipsum <span className={`${client==="programmer"?"colory":""}`}>web design</span> amet consectetur adipisicing elit. Voluptatibus
+      facere aperiam iste mollitia doloribus ad, quod odit natus amet sapiente,
+      laudantium libero molestiae doloremque cumque neque nihil sed eveniet
+      perferendis.
+    </p>
+  );
 }
-export default TextMin
+export default TextMin;
